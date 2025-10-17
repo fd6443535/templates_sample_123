a8a357c20857170ab5cb3a96dc74c5a80f1d56a9
fd6443535 <157208896+fd6443535@users.noreply.github.com>
2025-10-17 23:45:43 +0530

    Update config.js and fixed api_base access issue

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-const API_BASE = "http://localhost:4000";
+//const API_BASE = "http://localhost:4000";
+// config.js
+window.API_BASE = "https://req-for-gate.vercel.app";
+API_BASE = window.API_BASE; // makes API_BASE globally available
